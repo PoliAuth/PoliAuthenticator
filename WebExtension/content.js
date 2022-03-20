e1a956aa371a671cce0975631686cab78d3fd936
Stig Narnia <stig.narnia@gmail.com>
2022-03-20 20:09:17 +0100

    Added idbroker page skip

--- WebExtension/content.js
+++ WebExtension/content.js
@@ -17,42 +17,52 @@ if (window.location.href.includes("aunicalogin.polimi.it/aunicalogin/aunicalogin
 			'<br/><a href="#" onclick="performPoliAuth()" class="ingresso-federato-button ingresso-federato-button-size-m button-ingresso-federato"><span class="ingresso-federato-button-icon"><img src="http://matmacsystemfile.altervista.org/immagini/PoliAuthenticator_White.png" alt=""></span><span class="ingresso-federato-button-text">PoliAuthenticator</span></a>';
 			///aunicalogin/assets/cie/img/SVG/Logo_CIE_ID.svg
 		} catch (e){}
-
-
 	}, 100);
 }
 
 
 if (window.location.href.includes("webeep.polimi.it/my")) {
-            var name = 'poliAuthenticator';
-            const value = `; ${document.cookie}`;
-            const parts = value.split(`; ${name}=`);
-            var c = undefined;
-            if (parts.length === 2) {
-                c = parts.pop().split(';').shift();
-            }
-            if(c != undefined){
-								var newHTML = document.open("text/html", "replace");
-								newHTML.write("<h1>Redirect...</h1>");
-								newHTML.close();
-								setTimeout(function() {
-									var d = new Date()
-									d.setHours(d.getHours() - 2);
-									document.cookie = "poliAuthenticator=false;expires=" + d + ";domain=.polimi.it;path=/";
-                	c = undefined;
-                	name = 'poliAuthenticator_redirectUrl';
-                	const value = `; ${document.cookie}`;
-                	const parts = value.split(`; ${name}=`);
-                	var c = undefined;
-                	if (parts.length === 2) {
-                    	c = parts.pop().split(';').shift();
-                	}
-                	if(c != undefined){
-											document.cookie = "poliAuthenticator_redirectUrl=no;expires=" + d + ";domain=.polimi.it;path=/";
-                    	window.location.href = c;
-                	} else {
-										window.location.href = window.location.href
-									}
-								}, 1000);
-          	}
+	var name = 'poliAuthenticator';
+	const value = `; ${document.cookie}`;
+	const parts = value.split(`; ${name}=`);
+	var c = undefined;
+	if (parts.length === 2) {
+		c = parts.pop().split(';').shift();
+	}
+	if(c != undefined){
+		var newHTML = document.open("text/html", "replace");
+		newHTML.write("<h1>Redirect...</h1>");
+		newHTML.close();
+		setTimeout(function() {
+			var d = new Date()
+			d.setHours(d.getHours() - 2);
+			document.cookie = "poliAuthenticator=false;expires=" + d + ";domain=.polimi.it;path=/";
+			c = undefined;
+			name = 'poliAuthenticator_redirectUrl';
+			const value = `; ${document.cookie}`;
+			const parts = value.split(`; ${name}=`);
+			var c = undefined;
+			if (parts.length === 2) {
+				c = parts.pop().split(';').shift();
+			}
+			if (c != undefined){
+				document.cookie = "poliAuthenticator_redirectUrl=no;expires=" + d + ";domain=.polimi.it;path=/";
+				window.location.href = c;
+			} else {
+				window.location.href = window.location.href;
+			}
+		}, 1000);
+	}
+}
+
+
+if (window.location.href.includes("https://idbroker.webex.com/idb/saml2/jsp/doSSO.jsp")) {
+	setTimeout(function () {
+		try {
+			var button = document.getElementById('IDButton2');
+            document.getElementById('IDToken1').value = "diego.aldarese@mail.polimi.it";
+			button.removeAttribute('disabled');
+			button.click();
+		} catch (e){}
+	}, 100);
 }
